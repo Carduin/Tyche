1b10ffa71b174da304aaf322de9f40aa24f12de5
Arthur Murillo <arthurmurillo9@gmail.com>
2020-11-13 09:19:31 +0100

    Merged files

--- src/Tyche.php
+++ src/Tyche.php
@@ -1,6 +1,5 @@
 <?php
 header("Access-Control-Allow-Origin: *");
-include(dirname(__DIR__).'\src\DatabaseConnection.php');
 $server = "localhost";
 $databaseUsername = "arthur";
 $databasePassword = "arthur";
@@ -14,4 +13,22 @@ $animal = $dataBase->getRandomRow($nounsTable);
 $result = chop($adjective). " " . chop($animal);
 header('Content-Type: application/json');
 echo json_encode(array("generated_name" => $result));
-?>
+
+class DatabaseConnection {
+
+  private $connection;
+
+  function __construct($server, $username, $password, $db) {
+    $this->connection = mysqli_connect($server, $username, $password, $db);
+    if (!$this->connection) {
+        echo "Error : " . mysqli_connect_errno() . PHP_EOL;
+        exit;
+    }
+  }
+
+  function getRandomRow(string $tableName) {
+    $query = "SELECT * FROM $tableName ORDER BY RAND() LIMIT 1";
+    $result = $this->connection->query($query);
+    return mysqli_fetch_array($result)["name"];
+  }
+}
